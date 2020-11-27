059c5a2c277eb9560d25a0a3bf1c0899ebb51be9
AmandaRH07 <amandarafahass13@gmail.com>
2020-11-27 13:40:31 -0300

    Alteracoes

--- 2- Tintas.py
+++ 2- Tintas.py
@@ -1,13 +1,18 @@
 """
 Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. 
 Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, 
-que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.    Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
+que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.    
+Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
     comprar apenas latas de 18 litros;
     comprar apenas galões de 3,6 litros;
     misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
 
-"""
+150 metros quadrados
+2 latas
+7 galões
+ou 1 lata e 3 galões
 
+"""
 import exercises as cabecalho
 
 cabecalho.cabecalho("loja de tintas")
@@ -16,36 +21,36 @@ area_pintada = int(input("Insira o tamanho em metros quadrados a ser pintado: ")
 #resto = area_pintada - cont
 
 def lata(area_pintada):
-    #litro_metro2 = area_pintada + 6
-    #qtd_litros = 0
-
-    # cobertura de 1l para 6 m²
-    #for i in range(0, area_pintada, 6):
-    #    litro_metro2 -= 6
-    #   qtd_litros +=1
-
-    #latas = area_pintada/ 18
-    latas = area_pintada/(18 * 6)
+    latas = area_pintada/(18 * 6) * 1.1
+    latas = round(latas)
     valor_lata = latas * 80
     
-    print(f"O valor a ser pago para cobrir {area_pintada} m² utilizando {int(latas)} lata(s) de tinta é: {valor_lata:.2f}")
+    print(f"O valor a ser pago para cobrir {area_pintada} m² utilizando {latas} lata(s) de tinta é: {valor_lata:.2f}")
 
 def galao(area_pintada):
-    #litro_metro2 = area_pintada + 6
-    #qtd_litros = 0
-
-    # cobertura de 1l para 6 m²
-    #for i in range(0, area_pintada, 6):
-    #    litro_metro2 -= 6
-    #   qtd_litros +=1
-
-    #galao = area_pintada/ 18
-    galao = int(area_pintada/(3.6 * 6))
+    galao = area_pintada/(3.6 * 6) * 1.1
+    galao = round(galao)
     valor_galao = galao * 25
     
     print(f"O valor a ser pago para cobrir {area_pintada} m² utilizando {galao} galão(ões) de tinta é: {valor_galao:.2f}")
 
-#def lata_galao(area_pintada):
+def lata_galao(area_pintada):
+    latas = area_pintada/(18 * 6) * 1.1
+    print(latas)
+    latas = round(latas)
+    print(latas)
+
+    aux = area_pintada - (latas * (18 * 6))
+
+    galao = aux/(3.6 * 6) * 1.1
+    galao= round(galao)
+
+    valor_total = latas * 80  + galao * 25
+    print(f"""
+    O valor a ser pago para cobrir {area_pintada} m² de tinta é: {valor_total:.2f}
+    Você vai precisar de:
+    \tLatas: {latas}
+    \tGalões: {galao} """)
 
 while True:
     opcao = int(input(f"""
@@ -61,7 +66,7 @@ while True:
     elif opcao == 2:
         galao(area_pintada)
     elif opcao == 3:
-        pass
+        lata_galao(area_pintada)
     elif opcao == 4:
         print("Obrigado pela preferência :)")
         break
---
+++ 8- Poker.py
@@ -0,0 +1,26 @@
+""" 
+construa um analisador das 5 principais combinações de mãos do poker.
+Para isso utilize como base as classes descritas em:
+https://penseallen.github.io/PensePython2e/18-heranca.html
+considere como regra o poker fechado, em que a mão do jogador, já tem a combinação de 5 cartas :)
+
+par
+    Duas cartas com o mesmo valor.
+dois pares
+    Dois pares de cartas com o mesmo valor.
+trinca
+    Três cartas com o mesmo valor.
+sequência
+    Cinco cartas com valores em sequência (os ases  podem ser altos ou baixos, então Ace-2-3-4-5 é uma   sequência, assim como 10-Jack-Queen-King-Ace, mas     Queen-King-Ace-2-3 não é.)
+flush
+    Cinco cartas com o mesmo naipe.
+"""
+
+import exercises as cabecalho
+
+cabecalho.cabecalho("poker")
+
+
+
+
+
